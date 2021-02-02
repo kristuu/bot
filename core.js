a840651d870309232d7d58e3d3535a3c4bdf0627
Kristians Artis Berzins <kristiansberzins@outlook.lv>
2021-02-02 23:45:47 +0200

    pievienota funkcija dm

--- core.js
+++ core.js
@@ -1,17 +1,58 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('message', msg => {
-    if (msg.guild === null) {
-        msg.reply('');
+client.on('message', message => {
+    const prefix = '/';
+
+    if (message.guild === null) {
+        if (message.author != client.user.id) {
+        receivedm(message);
+        }
     } else {
-        if (msg.content == 'ping?') {
-                    var a = ['klasiska padla'];
-                    msg.channel.send(a[Math.floor(Math.random() * a.length)]);
-                    msg.delete()
+        if (message.content.startsWith(prefix)) {
+            if (message.content === prefix + 'dm') {
+                dm(message);
             }
+        } 
     }
-});
+})
+
+function dm(message) {
+	var admin = ["482586747201519617"]; //can dm users via bot, using the /dm command
+	if (admin.includes(message.author.id) == true) {
+		const split = message.content.split(">");
+		const targetid = split[1]
+		const letter = split[2]
+		if (targetid == "undefined" || targetid == null || targetid == "" && letter == "undefined" || letter == null || letter == "") {
+			message.channel.send("Command format: ``/dm,[USER_ID],[MESSAGE]``")
+		} else
+		if (targetid == "undefined" || targetid == null || targetid == "") {
+			message.channel.send("❗ Missing ``USER_ID``!\nType ``/dm`` to see the full command.")
+			message.react("❌")
+		} else
+		if (letter == "undefined" || letter == null || letter == "") {
+			message.channel.send("❗ Missing ``MESSAGE``!\nType ``/dm`` to see the full command.")
+			message.react("❌")
+		} else {
+			client.users
+				.fetch(targetid)
+				.then(user => {
+					user.send(letter)
+					message.react('✅')
+				})
+				.catch(err => {
+					if (err === "DiscordAPIError: 404: Not Found") {
+						message.channel.send("❗ Something went wrong! Refer to the error log below.\n\n``User with this ID hasn't been found! Error:\n" + err + '``')
+					} else {
+						message.channel.send('❗ Something went wrong! Refer to the error log below.\n\n``' + err + '``\n(Bot administrator contacted: <@746662409724231798>)')
+					}
+					message.react('❌')  
+    				})
+		}
+	} else {
+		message.channel.send("❗ Insufficient permissions")
+	}
+}
 
 client.on('message', message => {
     if (message.author.id === '482586747201519617') {
