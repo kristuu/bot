a3dfe15861f6c9949f4abd6065afb147bf8b7f09
Kristians Artis Berzins <kristiansberzins@outlook.lv>
2021-02-02 23:23:26 +0200

    update stunda

--- core.js
+++ core.js
@@ -16,7 +16,7 @@ client.on('message', msg => {
 client.on('message', message => {
     if (message.author.id === '482586747201519617') {
         if (message.content.startsWith('/stunda')) {
-            message.channel.send('@everyone');
+            // message.channel.send('@everyone');
             const embed = new Discord.MessageEmbed()
                 .setThumbnail('https://lh3.googleusercontent.com/proxy/bk3MS2-oJeoNu4GPT6-Cvn5GTt6q8MmO6gohbTYLBFVg1pl-wjBBTlX4NPjxJgDCe3Kpx6yoQesCSjF7wDkwovii2ichXU-BuAFeo1CIJ_d5')
                 .setTitle('Fizika')
@@ -24,10 +24,11 @@ client.on('message', message => {
                 .setURL('https://google.lv/')
                 .setFooter('ID: 1234 5678 9101, Passcode: 123456')
                 .addFields(
-                { name: 'Lai pievienotos stundai, spied uz zili iekrāsotā teksta', value: 'Sākums - 10:10', inline: false },
+                { name: 'SĀKUMS 10:10', value: 'Lai pievienotos stundai, spied uz zili iekrāsotā teksta', inline: true },
                 { name: 'Stundas tēma:', value: 'Praktiskais darbs - spēks un paātrinājums', inline: true }
-                )
-            message.channel.send(embed)
+                );
+            message.channel.send(embed);
+            message.delete();
         };
     };
 });
